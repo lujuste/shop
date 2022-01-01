b1ab6e359f21f22d25d3e0993ea24a9f1901efa0
Lucas Juste <lu.scatollini@hotmail.com>
2022-01-01 19:26:55 -0300

    32 user profile  update info

---
+++ pages/api/users/profile.ts
@@ -0,0 +1,34 @@
+import nc from 'next-connect'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import db from '../../../utils/db'
+import User from '../../../models/User'
+import bcrypt from 'bcryptjs'
+import { isAuth, signToken } from '../../../utils/auth'
+
+const handler = nc()
+handler.use(isAuth)
+
+handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
+  //@ts-ignore
+  await db.connect
+  //@ts-ignore
+  const user = await User.findById(req.user._id)
+  user.name = req.body.name
+  user.email = req.body.email
+  user.password = req.body.password
+    ? bcrypt.hashSync(req.body.password)
+    : user.password
+  await user.save()
+  await db.disconnect()
+
+  const token = signToken(user)
+  res.send({
+    token,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  })
+})
+
+export default handler
